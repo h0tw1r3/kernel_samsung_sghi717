5aed002eb8fddd7f79bd56061a10ba7ba5ecce31
Jeffrey Clark <dude@zaplabs.com>
2012-07-15 14:41:19 -0500

    cpufreq: add hotplug

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -419,6 +419,9 @@ extern struct cpufreq_governor cpufreq_gov_fantasy;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
+extern struct cpufreq_governor cpufreq_gov_hotplug;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
 #endif
 
 
